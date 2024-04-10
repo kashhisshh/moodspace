ee48205c4dd9e082a7dd9e547d3597459d49bfd9
Adarsh Kumar <54439573+Adarshkumar03@users.noreply.github.com>
2024-04-10 06:43:23 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,22 +3,14 @@ import MoodSparkline from "../MoodSparkline/MoodSparkline";
 import JournalList from "../JournalList/JournalList";
 import useAuthStore from "../../stores/authStore";
 import { useEffect, useState } from "react";
-import {
-  Container,
-  Grid,
-  Text,
-  Title,
-  Paper,
-  SimpleGrid,
-  rem,
-} from "@mantine/core";
+import { Container, Grid, Text, Title, Paper, SimpleGrid} from "@mantine/core";
 
 const moodMapping = {
-  1: { name: "Very Bad", color: "#d52941" },
-  2: { name: "Bad", color: "#504f9d" },
-  3: { name: "Neutral", color: "#e1f296" },
-  4: { name: "Good", color: "#70D560" },
-  5: { name: "Very Good", color: "#05372C" },
+  1: { name: "Critical", color: "#d52941" },
+  2: { name: "At-Risk", color: "#504f9d" },
+  3: { name: "Low", color: "#e1f296" },
+  4: { name: "Okay", color: "#70D560" },
+  5: { name: "Excellent", color: "#05372C" },
 };
 
 export default function Home() {
@@ -48,10 +40,11 @@ export default function Home() {
       });
 
       const ratings = data.moods.map((entry) => entry.rating);
+      const scores = data.moods.map((entry) => entry.smfqScore);
 
       const sum = ratings.reduce((total, rating) => total + rating, 0);
 
-      const averageRating = sum / ratings.length;
+      const averageRating = sum / data.moods.length;
       setAvg(averageRating);
 
       const formattedData = Object.entries(moodCounts).map(
@@ -68,7 +61,7 @@ export default function Home() {
       );
       setMaxMood(maxEntry);
       setMonthlyMoodData(formattedData);
-      setSparklineData(ratings);
+      setSparklineData(scores);
     };
     fetchData().catch((error) => {
       console.error("Error fetching data:", error);
@@ -95,7 +88,7 @@ export default function Home() {
                 {avg.toFixed(1)}
               </Title>
               <Text size="sm" fw={500} c="#DCD6F7">
-                Average Rating
+                Average Mood Rating
               </Text>
             </Paper>
           </Grid.Col>
@@ -105,7 +98,7 @@ export default function Home() {
                 {maxMood.value} times
               </Title>
               <Text size="sm" fw={500} c="#DCD6F7">
-                you had a {maxMood.name} mood
+                you had a <Text fw={700} c="white" span inherit>{maxMood.name}</Text> mood
               </Text>
             </Paper>
           </Grid.Col>
